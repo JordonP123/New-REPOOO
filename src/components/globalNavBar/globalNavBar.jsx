f9c7f2dd0607de018f54df81d5360b64b462abe0
JordonP123 <jordonaren@gmail.com>
2022-09-29 04:43:46 -0400

    added logos and rearanged skills (not an english major) updated background color, working on aboutMe

--- src/components/globalNavBar/globalNavBar.jsx
+++ src/components/globalNavBar/globalNavBar.jsx
@@ -1,16 +1,20 @@
-
-
 const GlobalNavBar = () => {
+  return (
+    <div className="globalNavBar">
+      <a href="#home" className="headerText">
+        Home
+      </a>
+      <a href="#aboutMe" className="headerText">
+        About Me
+      </a>
+      <a href="#projects" className="headerText">
+        Projects
+      </a>
+      <a href="#contact" className="headerText">
+        Contact Me
+      </a>
+    </div>
+  );
+};
 
-    return(
-        <div className='globalNavBar'>
-       <a href = '#home' className='headerText'>Home</a>
-       <a href='#aboutMe' className='headerText'>About Me</a>
-       <a href='#projects' className='headerText'>Projects</a>
-       <h2 className='headerText'>Contact Me</h2>
-        </div>
-    )
-}
-
-
-export default GlobalNavBar
+export default GlobalNavBar;
--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,44 +1,68 @@
-
-
 const Skills = () => {
-    return (
-        <div className='skillsContainer'>
-                <h1 className='skillsHeader'>My Skills</h1>
-                    <h2 className='frontEnd'>Front-end</h2>
-                        <div className='theLine'></div>
-                        <div className='theFiller frontEndFiller'></div>
-                    <h2 className='backEnd'>Back-end</h2>
-                        <div className='theLine'></div>
-                        <div className='theFiller backEndFiller'></div>
-                    <h2 className='backEnd'>React-JS</h2>
-                        <div className='theLine'></div>
-                        <div className='theFiller reactJsFiller'></div>
-                    <h2 className='backEnd'>Node-JS</h2>
-                        <div className='theLine'></div>
-                        <div className='theFiller nodeJsFiller'></div>
-                <div className='proficientBox'>
-                    <div className="mySkillsContainer">
-                        <h2 className='proficientHeader'>Proficient with : </h2>
-                        <div className="proficientContainer">
-                            <div className='mySkills mySkills2'>ReactJS</div>
-                            <div className='mySkills mySkills2'>JavaScript</div>
-                            <div className='mySkills mySkills2'>HTML</div>
-                            <div className='mySkills mySkills2'>CSS</div>
-                            <div className='mySkills mySkills2'>NodeJS</div>
-                            <div className='mySkills mySkills2'>Express</div>
-                    </div>
-                    </div>
-                    <div className="proficientContainer proficientContainer2">
-                    <h2 className='proficientHeader proficientHeader2'>Improving, but understand : </h2>
-                        <div className='mySkills mySkillsImproving'>Redux</div>
-                        <div className='mySkills mySkillsImproving'>Python</div>
-                        <div className='mySkills mySkillsImproving'>SQL</div>
-                        <div className='mySkills mySkillsImproving'>MongoDB</div>
-                    </div>
-                </div>
-
+  return (
+    <div className="skillsContainer">
+      <h1 className="skillsHeader">My Skills</h1>
+      <h2 className="frontEnd">Front-end</h2>
+      <div className="theLine"></div>
+      <div className="theFiller frontEndFiller"></div>
+      <h2 className="backEnd">Back-end</h2>
+      <div className="theLine"></div>
+      <div className="theFiller backEndFiller"></div>
+      <h2 className="backEnd">React-JS</h2>
+      <div className="theLine"></div>
+      <div className="theFiller reactJsFiller"></div>
+      <h2 className="backEnd">Node-JS</h2>
+      <div className="theLine"></div>
+      <div className="theFiller nodeJsFiller"></div>
+      <section className="proficientBox">
+        <h2 className="proficientHeader">Frontend / Backend </h2>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-react"></i>ReactJS
+          <div className="skillLevel">Advanced</div>
         </div>
-    )
-}
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-js"></i>JavaScript
+          <div className="skillLevel">Advanced</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-html5"></i>HTML
+          <div className="skillLevel">Advanced</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-css3-alt"></i>CSS
+          <div className="skillLevel">Advanced</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-node"></i>NodeJS
+          <div className="skillLevel">Advanced</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-node-js"></i>Express
+          <div className="skillLevel">Advanced</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-react"></i>Redux
+          <div className="skillLevel">Intermediate</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-solid fa-list-check"></i>Jest
+          <div className="skillLevel">Intermediate</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-solid fa-database"></i>SQL
+          <div className="skillLevel">Intermediate</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-node-js"></i>Knex
+          <div className="skillLevel">Intermediate</div>
+        </div>
+        <div className="mySkills">
+          <i class="skillsIcons fa-brands fa-python"></i>Python
+          <div className="skillLevel">Beginner</div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
